a5deedd22f39a9bda3f268d8d07b2e3fb34b93d1
Rexie18 <ro@students.kiron.ngo>
2017-03-01 20:03:03 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -71,9 +71,27 @@ var switchMenuToActive = function () {
 };
 
 // On page load (before images or CSS)
-document.addEventListener("DOMContentLoaded", function (event) {
 
+  document.addEventListener("DOMContentLoaded", function (event) {
+
+  // TODO: STEP 0: Look over the code from
+// *** start ***
+// to
+// *** finish ***
+// below.
+// We changed this code to retrieve all categories from the server instead of
+// simply requesting home HTML snippet. We now also have another function
+// called buildAndShowHomeHTML that will receive all the categories from the server
+// and process them: choose random category, retrieve home HTML snippet, insert that
+// random category into the home HTML snippet, and then insert that snippet into our
+// main page (index.html).
+//
+// TODO: STEP 1: Substitute [...] below with the *value* of the function buildAndShowHomeHTML,
+// so it can be called when server responds with the categories data.
+
+// *** start ***
 // On first load, show home view
+  
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   homeHtml,
@@ -81,8 +99,12 @@ $ajaxUtils.sendGetRequest(
     document.querySelector("#main-content")
       .innerHTML = responseText;
   },
-  false);
+  true);
 });
+  // ***** <---- TODO: STEP 1: Substitute [...] ******
+  //true); // Explicitely setting the flag to get JSON from server processed into an object literal
+});
+// *** finish **
 
 // Load the menu categories view
 dc.loadMenuCategories = function () {
