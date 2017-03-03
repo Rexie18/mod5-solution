334241f08bd9b39daaa8a740b7fb8b2f8ee297d1
Rexie18 <ro@students.kiron.ngo>
2017-03-03 11:08:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,12 +83,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
-  },
-  false); // ***** <---- TODO: STEP 1: Substitute [...] ******
- // Explicitely setting the flag to get JSON from server processed into an object literal
+  ["categoriesTitleHtml", "categoryHtml", "menuItemsTitleHtml"], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
